f8e18c9aa9dcd0d1cf605a402d2ef4398b828dd8
SjoukeHoekstra <sjouke.hoekstra@outlook.com>
2025-02-08 01:51:23 +0100

    flash

--- src/overworld.c
+++ src/overworld.c
@@ -1055,6 +1055,8 @@ static bool8 CanLearnFlashInParty(void)
 // Flash level of 8 is fully black
 void SetDefaultFlashLevel(void)
 {
+    if (CheckBagHasItem(ITEM_HM05_FLASH ,1) && CanLearnFlashInParty())
+        FlagSet(FLAG_SYS_USE_FLASH);
     if (!gMapHeader.cave)
         gSaveBlock1Ptr->flashLevel = 0;
     else if (FlagGet(FLAG_SYS_USE_FLASH))
