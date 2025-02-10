c856a2115dd3421ccfc08eadb3be9232dd9a99e1
SjoukeHoekstra <sjouke.hoekstra@outlook.com>
2025-02-10 22:40:01 +0100

    flash_fix_maybe?!

--- src/overworld.c
+++ src/overworld.c
@@ -72,6 +72,8 @@
 #include "constants/trainer_hill.h"
 #include "constants/weather.h"
 #include "randomizer.h"
+#include "item.h"
+#include "constants/items.h"
 
 struct CableClubPlayer
 {
@@ -176,6 +178,7 @@ static void TransitionMapMusic(void);
 static u8 GetAdjustedInitialTransitionFlags(struct InitialPlayerAvatarState *, u16, u8);
 static u8 GetAdjustedInitialDirection(struct InitialPlayerAvatarState *, u8, u16, u8);
 static u16 GetCenterScreenMetatileBehavior(void);
+static bool8 CanLearnFlashInParty(void);
 
 static void *sUnusedOverworldCallback;
 static u8 sPlayerLinkStates[MAX_LINK_PLAYERS];
@@ -1031,6 +1034,20 @@ bool32 Overworld_IsBikingAllowed(void)
     else
         return TRUE;
 }
+
+static bool8 CanLearnFlashInParty(void)
+{
+    u8 i;
+    for (i = 0; i < PARTY_SIZE; i++)
+    {
+        if (!GetMonData(&gPlayerParty[i], MON_DATA_SPECIES, NULL))
+            break;
+        if (!GetMonData(&gPlayerParty[i], MON_DATA_IS_EGG) && CanLearnTeachableMove(GetMonData(&gPlayerParty[i], MON_DATA_SPECIES), ITEM_HM05 - ITEM_TM01))
+            return TRUE;
+    }
+    return FALSE;
+}
+
 
 // Flash level of 0 is fully bright
 // Flash level of 1 is the largest flash radius
