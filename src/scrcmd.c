db5b2f61dcf30fee921194ed90aa4f1c087459b2
SjoukeHoekstra <sjouke.hoekstra@outlook.com>
2025-02-17 17:55:15 +0100

    src/scrcmd.c

--- src/scrcmd.c
+++ src/scrcmd.c
@@ -54,6 +54,7 @@
 #include "list_menu.h"
 #include "malloc.h"
 #include "constants/event_objects.h"
+#include "randomizer.h"
 
 typedef u16 (*SpecialFunc)(void);
 typedef void (*NativeFunc)(struct ScriptContext *ctx);
@@ -2034,6 +2035,14 @@ bool8 ScrCmd_setwildbattle(struct ScriptContext *ctx)
     u8 level2 = ScriptReadByte(ctx);
     u16 item2 = ScriptReadHalfword(ctx);
 
+    #if RANDOMIZER_AVAILABLE == TRUE
+        u8 mapNum = gSaveBlock1Ptr->location.mapNum;
+        u8 mapGroup = gSaveBlock1Ptr->location.mapGroup;
+        u8 localId = gObjectEvents[gSelectedObjectEvent].localId;
+
+        species = RandomizeFixedEncounterMon(species, mapNum, mapGroup, localId);
+    #endif
+
     if(species2 == SPECIES_NONE)
     {
         CreateScriptedWildMon(species, level, item);
@@ -2041,6 +2050,10 @@ bool8 ScrCmd_setwildbattle(struct ScriptContext *ctx)
     }
     else
     {
+        #if RANDOMIZER_AVAILABLE == TRUE
+            species2 = RandomizeFixedEncounterMon(species2, mapNum, mapGroup, localId);
+        #endif
+
         CreateScriptedDoubleWildMon(species, level, item, species2, level2, item2);
         sIsScriptedWildDouble = TRUE;
     }
