390794c99b437e8030ca4da0956daf77a4ec88b9
SjoukeHoekstra <sjouke.hoekstra@outlook.com>
2025-02-26 17:51:23 +0100

    endless_attempt

--- include/constants/items.h
+++ include/constants/items.h
@@ -996,11 +996,12 @@
 
 #define ITEM_INFINITE_REPEL 829
 #define ITEM_SUPER_CANDY 830
+#define ITEM_ENDLESS_CANDY 831
 
 // HOPO BERRY
 // LEGEND PLATE
 
-#define ITEMS_COUNT 831
+#define ITEMS_COUNT 832
 #define ITEM_FIELD_ARROW ITEMS_COUNT
 
 // A special item id associated with "Cancel"/"Exit" etc. in a list of items or decorations
